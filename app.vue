566b4bdc79ec8aac1758000b1e7f3f066f5623f2
Trey Turner <treyturner@users.noreply.github.com>
2023-04-21 03:12:52 -0500

    Initial commit

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="bg-black h-screen">
+    <div class="min-h-full flex items-center justify-center">
+      <div>
+        <img class="" src="~/public/img/logo.jpg" alt="logo" />
+        <h2 class="text-3xl text-white text-center">
+          Here's a great big heading, ok...
+        </h2>
+      </div>
+    </div>
+  </div>
+</template>
